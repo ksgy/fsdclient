1e7938a2cbe7784e490ca1139d07c16753a4db7d
ksgy <ksgy@x-plane.hu>
2016-02-26 23:35:05 +0000

    Sending works; Sending data every 1 second

--- LuaScripts/fsdclient.lua
+++ LuaScripts/fsdclient.lua
@@ -4,27 +4,21 @@ homebase = "LHBP"
 
 
 
-DataRef( "HH_heading", "sim/cockpit/autopilot/heading", "readonly" )
--- transponder flag
--- sqawk
--- alt
--- lat
--- lon
--- spd
--- hdg
-
-last_heading = HH_heading
+DataRef( "xpdr_f", "sim/cockpit/radios/transponder_mode", "readonly" )
+DataRef( "xpdr", "sim/cockpit/radios/transponder_code", "readonly" )
+DataRef( "alt", "sim/cockpit2/gauges/indicators/altitude_ft_pilot", "readonly" )
+DataRef( "lat", "sim/flightmodel/position/latitude", "readonly" )
+DataRef( "lon", "sim/flightmodel/position/longitude", "readonly" )
+DataRef( "spd", "sim/cockpit2/gauges/indicators/airspeed_kts_pilot", "readonly" )
+DataRef( "hdg", "sim/cockpit/autopilot/heading", "readonly" )
 
 
 function fsdclient()
-
-
 	infofile = io.open(SCRIPT_DIRECTORY.."fsdclient.txt", "w")
 	io.output(infofile)
-	io.write('heading: ' .. last_heading)
+	io.write(xpdr_f .. "|" .. xpdr .. "|" .. alt .. "|" .. lat .. "|" .. lon .. "|" .. spd .. "|" .. hdg .. "|" .. callsign .. "|" .. password .. "|" .. homebase)
 	io.close(infofile)
-	
 end
 
 -- do_sometimes("fsdclient()")
-do_every_frame("fsdclient()")
+do_often("fsdclient()")
--- modules/messages.py
+++ modules/messages.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import time
+import os
 
 # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,11 +20,32 @@ class Messages:
 
 		while run:
 			try:
+				with open(os.path.join('i:/','XPlane10','Resources','plugins','FlyWithLua','Scripts', 'fsdclient.txt'), 'r') as f:
+					flightinfo = f.readline()
+
+				# xpdr_f .. "|" .. xpdr .. "|" .. alt .. "|" .. lat .. "|" .. lon .. "|" .. spd .. "|" .. hdg)
+
+				s = flightinfo.split('|')
+				print flightinfo
+				xpdr_f = 'S'
+				if (s[0] == '2'):
+					xpdr_f = 'N'					
+
+				xpdr = s[1] 
+				alt = s[2]
+				lat = s[3]
+				lon = s[4]
+				spd = s[5]
+				hdg = s[6]
+				callsign = s[7]
+				password = s[8]
+				homebase = s[9]
 				# @<transponder flag>:<callsign&gt:<squawk code>:1:<latitude>:<longitude>:<altitude>:<speed>:<heading>:0
-				message = '@S:MIS:1200:1:19.424633:-99.077919:7335:100:4290775416:-329\r\n'
-				# print >>sys.stderr, 'sending "%s"' % message
+
+				message = '@'+xpdr_f+':'+callsign+':'+xpdr+':1:'+('{0:.6f}'.format(float(lat)))+':'+('{0:.6f}'.format(float(lon)))+':'+str(int(float(alt)))+':'+str(int(float(spd)))+':4290775416:-329\r\n'
+				print >>sys.stderr, 'sending "%s"' % message
 				sock.sendall(message)
-				time.sleep( 5 )
+				time.sleep(1)
 
 			except socket.timeout:
 				print("timeout error")
